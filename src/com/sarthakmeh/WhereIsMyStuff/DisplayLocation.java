7b1a28c61c13d84dd95a956d5a5a7b33e58540e5
sarthakmeh03 <sarthakmeh03@gmail.com>
2014-01-10 23:44:22 +0530

    requesting location in intervals

--- src/com/sarthakmeh/WhereIsMyStuff/DisplayLocation.java
+++ src/com/sarthakmeh/WhereIsMyStuff/DisplayLocation.java
@@ -4,6 +4,7 @@ package com.sarthakmeh.WhereIsMyStuff;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.GoogleMap;
 
 import com.google.android.gms.maps.SupportMapFragment;
@@ -14,6 +15,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.content.Intent;
 import android.location.Location;
+import android.location.LocationListener;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -23,20 +25,44 @@ import android.widget.Toast;
 
 
 public class DisplayLocation extends FragmentActivity implements GooglePlayServicesClient.ConnectionCallbacks,
-GooglePlayServicesClient.OnConnectionFailedListener {
+GooglePlayServicesClient.OnConnectionFailedListener,LocationListener {
 	
 	GoogleMap googleMap;
 	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
 	static final LatLng KIEL = new LatLng(53.551, 9.993);
 	LocationClient mLocationClient;
 	Location mCurrentLocation;
+	LocationRequest mLocationRequest;
+	String userLocation;
+	String newuserLocation;
+	
+	// Milliseconds per second
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+    // Update frequency in seconds
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+    // Update frequency in milliseconds
+    private static final long UPDATE_INTERVAL =
+            MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
+    // The fastest update frequency, in seconds
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+    // A fast frequency ceiling in milliseconds
+    private static final long FASTEST_INTERVAL =
+            MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mapvie);   
        
-               mLocationClient=new LocationClient(this, this, this);                    
+               mLocationClient=new LocationClient(this, this, this);
+               mLocationRequest=LocationRequest.create();
+               
+               mLocationRequest.setPriority(
+                       LocationRequest.PRIORITY_HIGH_ACCURACY);
+               // Set the update interval to 5 seconds
+               mLocationRequest.setInterval(UPDATE_INTERVAL);
+               // Set the fastest update interval to 1 second
+               mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
 
 //        try {
 //            // Loading map
@@ -108,9 +134,10 @@ GooglePlayServicesClient.OnConnectionFailedListener {
 	public void onConnected(Bundle bundle) {
 		// TODO Auto-generated method stub
 		mCurrentLocation = mLocationClient.getLastLocation();
-      Log.d("Location",Double.toString(mCurrentLocation.getLatitude()));
-      Log.d("Location",Double.toString(mCurrentLocation.getLongitude()));
-		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+      userLocation="Latitude"+Double.toString(mCurrentLocation.getLatitude())+"Longitude"+
+    		  Double.toString(mCurrentLocation.getLongitude());
+      mLocationClient.requestLocationUpdates(mLocationRequest, (com.google.android.gms.location.LocationListener) this);
+		Toast.makeText(this, userLocation, Toast.LENGTH_SHORT).show();
 		
 	}
 
@@ -119,6 +146,33 @@ GooglePlayServicesClient.OnConnectionFailedListener {
 		// TODO Auto-generated method stub
 		Toast.makeText(this, "Disconnected. Please re-connect.",
                 Toast.LENGTH_SHORT).show();
+		
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		
+		newuserLocation="Latitude"+Double.toString(mCurrentLocation.getLatitude())+"Longitude"+
+	    		  Double.toString(mCurrentLocation.getLongitude());
+		Toast.makeText(getApplicationContext(), newuserLocation, Toast.LENGTH_LONG).show();
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
 		
 	}          
 	}
