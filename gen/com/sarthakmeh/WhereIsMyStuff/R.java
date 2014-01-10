d0c4ad1b283cac3c159546b6d337a74f83728e9d
sarthakmeh03 <sarthakmeh03@gmail.com>
2014-01-11 01:39:53 +0530

    getting address from long and lat

--- gen/com/sarthakmeh/WhereIsMyStuff/R.java
+++ gen/com/sarthakmeh/WhereIsMyStuff/R.java
@@ -208,15 +208,18 @@ containing a value of this type.
         public static final int ic_plusone_tall_off_client=0x7f02001c;
     }
     public static final class id {
-        public static final int action_settings=0x7f05000f;
-        public static final int device_address=0x7f05000d;
-        public static final int device_name=0x7f05000c;
+        public static final int action_settings=0x7f050012;
+        public static final int address_progress=0x7f05000d;
+        public static final int currentLocation=0x7f05000c;
+        public static final int device_address=0x7f050011;
+        public static final int device_name=0x7f050010;
         public static final int foundlistview=0x7f05000b;
         public static final int hybrid=0x7f050004;
+        public static final int lastLocation=0x7f05000e;
         public static final int linearLayout1=0x7f050006;
         public static final int linearLayout2=0x7f050007;
         public static final int linearLayout3=0x7f050009;
-        public static final int map=0x7f05000e;
+        public static final int navigate=0x7f05000f;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int pairedlistview=0x7f050008;
@@ -231,8 +234,8 @@ containing a value of this type.
     public static final class layout {
         public static final int activity_homescreen=0x7f030000;
         public static final int activity_paireddevices=0x7f030001;
-        public static final int device_list=0x7f030002;
-        public static final int mapvie=0x7f030003;
+        public static final int activity_showlocation=0x7f030002;
+        public static final int device_list=0x7f030003;
     }
     public static final class menu {
         public static final int home_screen=0x7f0a0000;
--- src/com/sarthakmeh/WhereIsMyStuff/DisplayLocation.java
+++ src/com/sarthakmeh/WhereIsMyStuff/DisplayLocation.java
@@ -1,6 +1,10 @@
 package com.sarthakmeh.WhereIsMyStuff;
 
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
@@ -13,38 +17,52 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 
 public class DisplayLocation extends FragmentActivity implements GooglePlayServicesClient.ConnectionCallbacks,
-GooglePlayServicesClient.OnConnectionFailedListener,LocationListener {
+GooglePlayServicesClient.OnConnectionFailedListener,LocationListener, com.google.android.gms.location.LocationListener {
 	
-	GoogleMap googleMap;
-	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-	static final LatLng KIEL = new LatLng(53.551, 9.993);
+	
 	LocationClient mLocationClient;
 	Location mCurrentLocation;
 	LocationRequest mLocationRequest;
-	String userLocation;
-	String newuserLocation;
+	String currentUserLatitude;
+	String currentUserLongitude;
+	String TAG="WhereIsMyStuff";
+	TextView userLastLocation,usercurrentLocation;
+	ProgressBar mActivityIndicator;
+	SharedPreferences prefs;
+	Editor editPrefs;
+	String lastUserLatitude;
+	String lastUserLongitude;
+	boolean locationChanged=false;
 	
 	// Milliseconds per second
     private static final int MILLISECONDS_PER_SECOND = 1000;
     // Update frequency in seconds
-    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 300;
     // Update frequency in milliseconds
     private static final long UPDATE_INTERVAL =
             MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
     // The fastest update frequency, in seconds
-    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
+    private static final int FASTEST_INTERVAL_IN_SECONDS = 300;
     // A fast frequency ceiling in milliseconds
     private static final long FASTEST_INTERVAL =
             MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
@@ -52,8 +70,18 @@ GooglePlayServicesClient.OnConnectionFailedListener,LocationListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mapvie);   
+        setContentView(R.layout.activity_showlocation);   
        
+        usercurrentLocation=(TextView) findViewById(R.id.currentLocation);
+        userLastLocation=(TextView) findViewById(R.id.lastLocation);
+        mActivityIndicator=(ProgressBar) findViewById(R.id.address_progress);
+        
+        prefs=getSharedPreferences("UPDATE_LOCAION",Context.MODE_PRIVATE);
+          
+        	 lastUserLatitude=prefs.getString("Latitude",null);
+        	 lastUserLongitude=prefs.getString("Longitude",null);
+          
+        
                mLocationClient=new LocationClient(this, this, this);
                mLocationRequest=LocationRequest.create();
                
@@ -63,6 +91,8 @@ GooglePlayServicesClient.OnConnectionFailedListener,LocationListener {
                mLocationRequest.setInterval(UPDATE_INTERVAL);
                // Set the fastest update interval to 1 second
                mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+               
+               
 
 //        try {
 //            // Loading map
@@ -90,41 +120,43 @@ GooglePlayServicesClient.OnConnectionFailedListener,LocationListener {
     /**
      * function to load map. If map is not created it will create it for you
      * */
-    private void initilizeMap() {
-        if (googleMap == null) {
-            googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(
-                    R.id.map)).getMap();
- 
-            // check if map is created successfully or not
-            if (googleMap == null) {
-                Toast.makeText(getApplicationContext(),
-                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        }
-    }
- 
+//    private void initilizeMap() {
+//        if (googleMap == null) {
+//            googleMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(
+//                    R.id.map)).getMap();
+// 
+//            // check if map is created successfully or not
+//            if (googleMap == null) {
+//                Toast.makeText(getApplicationContext(),
+//                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+//                        .show();
+//            }
+//        }
+//    }
+// 
     @Override
     protected void onStart() {
         super.onStart();
         // Connect the client.
+        
         mLocationClient.connect();
+        Log.d(TAG,"LocationClient connected");
     }
     
     @Override
     protected void onStop() {
-        // Disconnecting the client invalidates it.
-        mLocationClient.disconnect();
+               
         super.onStop();
     }
     
     @Override
     protected void onResume() {
         super.onResume();
+       
      //   initilizeMap();
     }
 
-	@Override
+    @Override
 	public void onConnectionFailed(ConnectionResult arg0) {
 		// TODO Auto-generated method stub
 		
@@ -133,14 +165,24 @@ GooglePlayServicesClient.OnConnectionFailedListener,LocationListener {
 	@Override
 	public void onConnected(Bundle bundle) {
 		// TODO Auto-generated method stub
-		mCurrentLocation = mLocationClient.getLastLocation();
-      userLocation="Latitude"+Double.toString(mCurrentLocation.getLatitude())+"Longitude"+
-    		  Double.toString(mCurrentLocation.getLongitude());
-      mLocationClient.requestLocationUpdates(mLocationRequest, (com.google.android.gms.location.LocationListener) this);
-		Toast.makeText(this, userLocation, Toast.LENGTH_SHORT).show();
+		mCurrentLocation = mLocationClient.getLastLocation();	
+		
+   currentUserLatitude=Double.toString(mCurrentLocation.getLatitude());
+   currentUserLongitude=Double.toString(mCurrentLocation.getLongitude());
+   
+     if(lastUserLatitude==null && lastUserLongitude==null){
+    	 lastUserLatitude=currentUserLatitude;
+    	 lastUserLongitude=currentUserLongitude;
+     }
+     
+     new GetAddressTask(this).execute(currentUserLatitude,currentUserLongitude,lastUserLatitude,lastUserLongitude);
+     
+     mLocationClient.requestLocationUpdates(mLocationRequest, this);
+		
 		
 	}
-
+	
+	
 	@Override
 	public void onDisconnected() {
 		// TODO Auto-generated method stub
@@ -152,12 +194,79 @@ GooglePlayServicesClient.OnConnectionFailedListener,LocationListener {
 	@Override
 	public void onLocationChanged(Location location) {
 		
-		newuserLocation="Latitude"+Double.toString(mCurrentLocation.getLatitude())+"Longitude"+
-	    		  Double.toString(mCurrentLocation.getLongitude());
-		Toast.makeText(getApplicationContext(), newuserLocation, Toast.LENGTH_LONG).show();
+		SharedPreferences backgroundPrefs;
+		backgroundPrefs=getSharedPreferences("UPDATE_LOCAION",Context.MODE_PRIVATE);
+		Editor edit;
+		edit=backgroundPrefs.edit();
+		
+	String userLatitude=Double.toString(location.getLatitude());
+	String userLongitude=Double.toString(location.getLongitude());
+	    
+	    edit.putBoolean("Location Changed",true);
+	    edit.putString("Latitude",userLatitude);
+	    edit.putString("Longitude",userLongitude);
+	    edit.commit();
+	}
+	
+	
+	 class GetAddressTask extends AsyncTask<String, Void, Void> {
+         
+   	  Context mContext;
+   	  List<Address> currentAddress=null;
+   	  List<Address> lastAddress=null;
+    
+   	  public GetAddressTask(Context context) {
+             super();
+             mContext = context;
+      }
+   	  
+	@Override
+	protected Void doInBackground(String... str){
+		
+		Geocoder geocoder =
+                new Geocoder(mContext, Locale.getDefault());
+		
+		try {
+			currentAddress = geocoder.getFromLocation(Double.parseDouble(str[0]),
+			        Double.parseDouble(str[1]), 1);
+			lastAddress = geocoder.getFromLocation(Double.parseDouble(str[2]),
+			        Double.parseDouble(str[3]), 1);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (currentAddress != null && currentAddress.size() > 0) {
+            // Get the first address
+            Address address = currentAddress.get(0);
+            /*
+             * Format the first line of address (if available),
+             * city, and country name.
+             */
+            String addressText = String.format(
+                    "%s, %s, %s",
+                    // If there's a street address, add it
+                    address.getMaxAddressLineIndex() > 0 ?
+                            address.getAddressLine(0) : "",
+                    // Locality is usually a city
+                    address.getLocality(),
+                    // The country of the address
+                    address.getCountryName());
+            
+            Log.d(TAG,addressText);
+		
+            
 		
 	}
-
+		return null;
+    }
+	
+	 
+	 }
+	
 	@Override
 	public void onProviderDisabled(String provider) {
 		// TODO Auto-generated method stub
@@ -175,5 +284,6 @@ GooglePlayServicesClient.OnConnectionFailedListener,LocationListener {
 		// TODO Auto-generated method stub
 		
 	}          
-	}
+     
+}
 
